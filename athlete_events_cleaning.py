ba60e15fcf4483986b47c4f13f70ebd16724f1fb
Joel Ghahremani <joel.ghahremani@gmail.com>
2019-01-22 15:11:40 +0100

    Python file for adding birth years to new DB, & that CSV file.

--- athlete_events_cleaning.py
+++ athlete_events_cleaning.py
@@ -1,10 +1,28 @@
 import csv
-
+'''
 athletes_medal = []
 with open('athlete_events.csv', 'r') as athletes:
     with open('athletes_medals.csv', 'w') as file:
         for line in athletes:
             if line[-3:-1] != 'NA':
                 file.write(line)
+'''
+
+
+with open('athletes_medals.csv', 'r') as athletes:
+    reader = csv.DictReader(athletes)
+    headers = reader.fieldnames
+    headers.append('BirthYear')
+    #print(f'headers is of type: {type(headers)}')
+
+    with open('athletes_years.csv', 'w', newline ='') as athletesyears:
+        writer = csv.DictWriter(athletesyears, fieldnames=headers)
+        for line in reader: 
+            if line['Age'] != 'NA':
+                year = int(line['Year'])
+                age = int(line['Age'])
+                birthyear = year - age
+                line['BirthYear'] = birthyear
+                writer.writerow(line)
 
 
